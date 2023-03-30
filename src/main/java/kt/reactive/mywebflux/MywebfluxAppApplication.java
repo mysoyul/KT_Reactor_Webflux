b4867b484362594cf19fa6781c43fabb6fd82f4d
mysoyul <vega2k_@naver.com>
2023-03-30 10:49:03 +0900

    reactor webflux add

---
+++ src/main/java/kt/reactive/mywebflux/MywebfluxAppApplication.java
@@ -0,0 +1,13 @@
+package kt.reactive.mywebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebfluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebfluxAppApplication.class, args);
+	}
+
+}
