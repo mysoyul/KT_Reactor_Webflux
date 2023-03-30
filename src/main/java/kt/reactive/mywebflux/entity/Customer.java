dc18561ba63ed88c0d335d2088dec28b6be74172
mysoyul <vega2k_@naver.com>
2023-03-30 15:37:06 +0900

    entity add

--- src/main/java/kt/reactive/mywebflux/entity/Customer.java
+++ src/main/java/kt/reactive/mywebflux/entity/Customer.java
@@ -4,17 +4,20 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
-@Data @NoArgsConstructor
+
+@Data
+@NoArgsConstructor
 @Table
 public class Customer {
-@Id
-private Long id;
-@Column
-private String firstName;
-@Column
-private String lastName;
-public Customer(String firstName, String lastName) {
-this.firstName = firstName;
-this.lastName = lastName;
-}
+    @Id
+    private Long id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
