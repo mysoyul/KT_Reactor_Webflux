02149c9275b311464ab353937001629fc183f461
mysoyul <vega2k_@naver.com>
2023-03-31 13:39:24 +0900

    customer update add

--- src/main/java/kt/reactive/mywebflux/router/CustomerRouterFunction.java
+++ src/main/java/kt/reactive/mywebflux/router/CustomerRouterFunction.java
@@ -17,7 +17,8 @@ public class CustomerRouterFunction {
         return RouterFunctions
                 .route(GET("/router/r2customers"), customerHandler::getCustomers)
                 .andRoute(GET("/router/r2customers/{id}"), customerHandler::getCustomer)
-                .andRoute(POST("/router/r2customers"), customerHandler::saveCustomer);
+                .andRoute(POST("/router/r2customers"), customerHandler::saveCustomer)
+                .andRoute(PUT("/router/r2customers/{id}"), customerHandler::updateCustomer);
 //        return RouterFunctions.route(GET("/router/r2customers"), new HandlerFunction<ServerResponse>() {
 //            @Override
 //            public Mono<ServerResponse> handle(ServerRequest request) {
