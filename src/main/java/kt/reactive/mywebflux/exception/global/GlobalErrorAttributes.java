0b1611b834b800e6648efa2caa97ea9bbbf12a2a
mysoyul <vega2k_@naver.com>
2023-03-31 11:43:31 +0900

    global error add

--- src/main/java/kt/reactive/mywebflux/exception/global/GlobalErrorAttributes.java
+++ src/main/java/kt/reactive/mywebflux/exception/global/GlobalErrorAttributes.java
@@ -6,17 +6,22 @@ import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
+import java.net.InetSocketAddress;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class GlobalErrorAttributes extends DefaultErrorAttributes {
     @Override
-    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
+    public Map<String, Object> getErrorAttributes(ServerRequest request,
+                                                  ErrorAttributeOptions options) {
         Map<String, Object> errorMap = new HashMap<>();
         Throwable error = super.getError(request);
+
         if (error instanceof CustomAPIException) {
             CustomAPIException customAPIException = (CustomAPIException) error;
+            errorMap.put("timestamp", new Date());
             errorMap.put("message", customAPIException.getMessage());
             errorMap.put("status", customAPIException.getHttpStatus());
             errorMap.put("endpoint url", request.path());
