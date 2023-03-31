6bbf81a1eaa66e99c7ab958dc82ec989ca01a07c
mysoyul <vega2k_@naver.com>
2023-03-31 10:25:23 +0900

    router add

--- src/main/java/kt/reactive/mywebflux/router/CustomerRouterFunction.java
+++ src/main/java/kt/reactive/mywebflux/router/CustomerRouterFunction.java
@@ -3,9 +3,8 @@ package kt.reactive.mywebflux.router;
 import kt.reactive.mywebflux.handler.CustomerHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
@@ -15,6 +14,12 @@ public class CustomerRouterFunction {
     public RouterFunction<ServerResponse> routerFunction(CustomerHandler customerHandler) {
         //route() 메서드이 2번째 아규먼트 HandlerFunction
         // HandlerFunction의 추상메서드 reactor.core.publisher.Mono<T> handle(ServerRequest request)
-        return RouterFunctions.route(GET("/router/r2customers"), customerHandler::getCustomers);
+        //return RouterFunctions.route(GET("/router/r2customers"), customerHandler::getCustomers);
+        return RouterFunctions.route(GET("/router/r2customers"), new HandlerFunction<ServerResponse>() {
+            @Override
+            public Mono<ServerResponse> handle(ServerRequest request) {
+                return customerHandler.getCustomers(request);
+            }
+        });
     }
 }
