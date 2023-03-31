715d2ad57c3b0682f87860a5033ac273a5357dba
mysoyul <vega2k_@naver.com>
2023-03-31 14:24:25 +0900

    customer delete add

--- src/main/java/kt/reactive/mywebflux/handler/CustomerHandler.java
+++ src/main/java/kt/reactive/mywebflux/handler/CustomerHandler.java
@@ -70,11 +70,13 @@ public class CustomerHandler {
 
     public Mono<ServerResponse> deleteCustomer(ServerRequest request) {
         Long id = Long.parseLong(request.pathVariable("id"));
-        return customerRepository.findById(id)
+        Mono<ServerResponse> serverResponseMono = customerRepository.findById(id)
                 .flatMap(existCustomer ->
                         ServerResponse.ok()
                                 .build(customerRepository.delete(existCustomer)))
                 .switchIfEmpty(getError(id));
+        return serverResponseMono;
+
     }
 
 }
